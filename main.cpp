0f07190d27aaee49e8375c119f8d667e1af579a1
Schab <olaf.schab@nokia.com>
2019-07-18 11:06:01 +0200

    add missing main.cpp change

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "MonopolyGame.h"
+#include "Die.h"
 
 int main()
 {
-    MonopolyGame monopolyGame(40);
+    std::shared_ptr<DieInterface> die1 = std::make_shared<DieK6>();
+    std::shared_ptr<DieInterface> die2 = std::make_shared<DieK10>();
+    std::shared_ptr<DieBucket> dieBucket = std::make_shared<DieBucket>(die1, die2);
+    MonopolyGame monopolyGame(40, dieBucket);
     monopolyGame.addPlayer("Kuba");
     monopolyGame.addPlayer("Olaf");
     monopolyGame.addPlayer("Stee");
